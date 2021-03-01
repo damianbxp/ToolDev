51e5e314caaf40175cb2d02fcacc5459fe548069
Damian Bartuś <damianb.xp@gmail.com>
2021-03-01 01:12:44 +0100

    Added mirroring function System generates new gameobjects even if already exist

--- Assets/Editor/MirrorTool.cs
+++ Assets/Editor/MirrorTool.cs
@@ -10,12 +10,17 @@ public class MirrorTool : EditorWindow {
     public List<GameObject> sourceObjects;
     public Transform mirrorTf;
 
+    private List<GameObject> mirroredObjects;
+
     private bool sourceObjectsToogle;
     private Vector2 scrollPos;
 
     private bool mirrorX = false;
+    private bool lastMirrorX = false;
     private bool mirrorY = false;
+    private bool lastMirrorY = false;
     private bool mirrorZ = false;
+    private bool lastMirrorZ = false;
 
     private bool advancedSettingsToggle = false;
 
@@ -35,9 +40,11 @@ public class MirrorTool : EditorWindow {
 
     private List<int> toRemove;
 
-
+    SerializedObject so;
 
     private void OnEnable() {
+        so = new SerializedObject(this);
+
 
         Selection.selectionChanged += Repaint;
         SceneView.duringSceneGui += DuringSceneGUI;
@@ -48,9 +55,11 @@ public class MirrorTool : EditorWindow {
     }
 
     private void DuringSceneGUI(SceneView sceneView) {
+
+
         switch(Event.current.type) {
             case EventType.Repaint:
-                DrawMirrorGizmo();
+                DrawMirrorGizmos();
                 break;
             default:
                 break;
@@ -61,12 +70,21 @@ public class MirrorTool : EditorWindow {
     private void OnGUI() {
         using(new GUILayout.VerticalScope(EditorStyles.helpBox)) {
 
+            if(GUILayout.Button("MIRROR")) MirrorObjects();
+
             mirrorTf = EditorGUILayout.ObjectField("Mirror", mirrorTf, typeof(Transform), true) as Transform;
 
+            lastMirrorX = mirrorX;
+            lastMirrorY = mirrorY;
+            lastMirrorZ = mirrorZ;
 
             mirrorX = EditorGUILayout.Toggle("X", mirrorX);
             mirrorY = EditorGUILayout.Toggle("Y", mirrorY);
             mirrorZ = EditorGUILayout.Toggle("Z", mirrorZ);
+
+            if(mirrorX != lastMirrorX || mirrorY != lastMirrorY || mirrorZ != lastMirrorZ) {
+                SceneView.RepaintAll();
+            }
 
             advancedSettingsToggle = EditorGUILayout.Foldout(advancedSettingsToggle, "Advanced Settings");
 
@@ -115,7 +133,8 @@ public class MirrorTool : EditorWindow {
 
 
                     scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
-                    toRemove.Clear();
+                    if(toRemove.Count > 0)
+                        toRemove.Clear();
 
                     for(int i = 0; i < sourceObjects.Count; i++) {
                         using(new GUILayout.HorizontalScope()) {
@@ -123,7 +142,6 @@ public class MirrorTool : EditorWindow {
                             if(GUILayout.Button("X")) toRemove.Add(i);
                         }
                     }
-
 
                     if(toRemove.Count > 0) RemoveObjects();
                     EditorGUILayout.EndScrollView();
@@ -135,16 +153,69 @@ public class MirrorTool : EditorWindow {
         }
     }
 
+    private void MirrorObjects() {
+        foreach(GameObject sourceObject in sourceObjects) {
+            if(false) {
+                GameObject mirroredObject = Instantiate<GameObject>(sourceObject, Vector3.zero, Quaternion.identity);
+                mirroredObject.name += " (Mirrored)";
+
+                mirroredObjects.Add(mirroredObject);
+                Debug.Log(sourceObjects.Count + "|" + mirroredObjects.Count);
+            }
+        }
+        for(int i = 0; i < mirroredObjects.Count; i++) {
+            mirroredObjects[i].transform.position = GetMirroredPosition(sourceObjects[i].transform.position);
+        }
+
+        foreach(GameObject source in sourceObjects) {
+            Debug.DrawLine(GetMirroredPosition(source.transform.position), source.transform.position, Color.green);
+        }
+    }
+
+    private Vector3 GetMirroredPosition(Vector3 originalPos) {
+        Vector3 mirrorPoint = mirrorTf.position;
+        float lastDistance;
+        float checkDelta = 0.1f;
+
+        lastDistance = Vector3.Distance(mirrorPoint, originalPos);
+
+        if(lastDistance > Vector3.Distance(mirrorPoint + mirrorTf.forward * checkDelta, originalPos)) {
+            mirrorPoint = FindMirrorPoint(originalPos, 1, checkDelta);
+        }
+        else if(lastDistance > Vector3.Distance(mirrorPoint - mirrorTf.forward * checkDelta, originalPos)) {
+            mirrorPoint = FindMirrorPoint(originalPos, -1, checkDelta);
+        }
+
+        mirrorPoint.y = originalPos.y;
+
+        return 2 * mirrorPoint - originalPos;
+        
+    }
+
+    private Vector3 FindMirrorPoint(Vector3 originalPos, int checkDirection, float checkDelta) {
+        Vector3 mirrorPoint = mirrorTf.position;
+        float lastDistance;
+
+        do {
+            lastDistance = Vector3.Distance(mirrorPoint, originalPos);
+            mirrorPoint += mirrorTf.forward * checkDirection * checkDelta;
+
+        } while(Vector3.Distance(mirrorPoint + mirrorTf.forward * checkDirection * checkDelta, originalPos) < lastDistance);
+
+        return mirrorPoint;
+    }
+
     private void SetDefaultParams() {
         gizmoRectHeight = 1f;
         gizmoRectWidth = 2f;
 
-        mirrorXColor = new Color(1f, 0f, 0f, 0.3f);
+        mirrorXColor = new Color(1f, 0f, 0f, 0.1f);
         mirrorXColorOutline = new Color(1f, 0f, 0f, 0.8f);
-        mirrorYColor = new Color(0f, 1f, 0f, 0.3f);
+        mirrorYColor = new Color(0f, 1f, 0f, 0.1f);
         mirrorYColorOutline = new Color(0f, 1f, 0f, 0.8f);
-        mirrorZColor = new Color(0f, 0f, 1f, 0.3f);
+        mirrorZColor = new Color(0f, 0f, 1f, 0.1f);
         mirrorZColorOutline = new Color(0f, 0f, 1f, 0.8f);
+        SceneView.RepaintAll();
     }
 
     private void AddSourceObjects() {
@@ -157,25 +228,27 @@ public class MirrorTool : EditorWindow {
 
     private void CleanMirror() {
         sourceObjects.Clear();
+        mirroredObjects.Clear();
     }
 
     private void RemoveObjects() {
         foreach(int i in toRemove) {
             sourceObjects.RemoveAt(i);
+            mirroredObjects.RemoveAt(i);
         }
     }
 
-    private void DrawMirrorGizmo() {
+    private void DrawMirrorGizmos() {
         if(mirrorTf != null) {
-            if(mirrorX) DrawMirrorGizmosX();
-            if(mirrorY) DrawMirrorGizmosY();
-            if(mirrorZ) DrawMirrorGizmosZ();
+            if(mirrorX) DrawMirrorGizmoX();
+            if(mirrorY) DrawMirrorGizmoY();
+            if(mirrorZ) DrawMirrorGizmoZ();
         } else {
             Debug.LogWarning("Mirror object Missing");
         }
     }
 
-    private void DrawMirrorGizmosX() {
+    private void DrawMirrorGizmoX() {
         Vector3[] rectVerts = new Vector3[] {
                 mirrorTf.position + mirrorTf.forward * (gizmoRectWidth/2) + mirrorTf.up * (gizmoRectHeight/2),
                 mirrorTf.position + mirrorTf.forward * (gizmoRectWidth/2) - mirrorTf.up * (gizmoRectHeight/2),
@@ -185,7 +258,7 @@ public class MirrorTool : EditorWindow {
         Handles.DrawSolidRectangleWithOutline(rectVerts, mirrorXColor, mirrorXColorOutline);
     }
 
-    private void DrawMirrorGizmosY() {
+    private void DrawMirrorGizmoY() {
         Vector3[] rectVerts = new Vector3[] {
                 mirrorTf.position + mirrorTf.forward * (gizmoRectWidth/2) + mirrorTf.right * (gizmoRectHeight/2),
                 mirrorTf.position + mirrorTf.forward * (gizmoRectWidth/2) - mirrorTf.right * (gizmoRectHeight/2),
@@ -196,7 +269,7 @@ public class MirrorTool : EditorWindow {
         Handles.DrawSolidRectangleWithOutline(rectVerts, mirrorYColor, mirrorYColorOutline);
     }
 
-    private void DrawMirrorGizmosZ() {
+    private void DrawMirrorGizmoZ() {
         Vector3[] rectVerts = new Vector3[] {
                 mirrorTf.position + mirrorTf.up * (gizmoRectWidth/2) + mirrorTf.right * (gizmoRectHeight/2),
                 mirrorTf.position + mirrorTf.up * (gizmoRectWidth/2) - mirrorTf.right * (gizmoRectHeight/2),
