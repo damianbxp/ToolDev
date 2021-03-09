53f54ece7c715e434a98f5927ad6200a973516fc
Damian Bartuś <damianb.xp@gmail.com>
2021-03-09 02:48:29 +0100

    imported lowpoly city prefabs added docfx added basic construction for fence builder

---
+++ Assets/Editor/FenceTool/Fence.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+/// <summary>
+/// Holds data about fence
+/// </summary>
+public class Fence : Editor
+{
+    
+}
---
+++ Assets/Editor/FenceTool/FenceBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Fence Builder Window
+/// </summary>
+
+public class FenceBuilder : EditorWindow
+{
+    [MenuItem("Tools/Fence Builder")]
+    public static void OpenFenceBuilder() => GetWindow<FenceBuilder>("Fence Builder");
+    /// <summary>
+    /// Stores all fences
+    /// </summary>
+    private List<Fence> fences;
+    
+    private GameObject fencesMaster;
+    /// <value>
+    /// Name of fence master
+    /// </value>
+    private const string fenceMasterName = "Fences Master";
+
+    private void OnEnable() {
+        fencesMaster = GameObject.Find(fenceMasterName);
+        SceneView.duringSceneGui += DuringSceneGUI;
+    }
+    private void OnDisable() {
+        SceneView.duringSceneGui -= DuringSceneGUI;
+    }
+
+    private void DuringSceneGUI(SceneView sceneView) {
+
+    }
+
+    private void OnGUI() {
+        using( new GUILayout.VerticalScope(EditorStyles.helpBox)) {
+            if(GUILayout.Button("Create Fence")) CreateFence();
+        }
+    }
+    /// <summary>
+    /// Creates New Fence. Adds new Fence Master if needed
+    /// </summary>
+    private void CreateFence() {
+        if(fencesMaster != null && GameObject.Find(fenceMasterName) != null) {
+            fencesMaster = GameObject.Find(fenceMasterName);
+        } else {
+            fencesMaster = new GameObject(fenceMasterName);
+            fencesMaster.isStatic = true;
+        }
+
+
+    }
+}
