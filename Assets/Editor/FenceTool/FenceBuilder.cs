3c538f5eb04bedfee61eb2bef7079d1e3f8f6deb
Damian Bartuś <damianb.xp@gmail.com>
2021-03-15 02:42:54 +0100

    Added feature to create fences and waypoints in selected fence

--- Assets/Editor/FenceTool/FenceBuilder.cs
+++ Assets/Editor/FenceTool/FenceBuilder.cs
@@ -15,10 +15,11 @@ public class FenceBuilder : EditorWindow
     [MenuItem("Tools/Fence Builder")]
     public static void OpenFenceBuilder() => GetWindow<FenceBuilder>("Fence Builder");
 
-    private List<Fence> fences;
-    
     private GameObject fencesMaster;
 
+    private string newFenceName;
+    private int editFenceId;
+    private Vector2 scrollPos;
     private const string fenceMasterName = "Fences Master";
 
     private void OnEnable() {
@@ -28,22 +29,37 @@ public class FenceBuilder : EditorWindow
     private void OnDisable() {
         SceneView.duringSceneGui -= DuringSceneGUI;
     }
-    /// <summary>
-    /// Being called whem active scene redraws
-    /// </summary>
-    private void DuringSceneGUI(SceneView sceneView) {
 
+    private void DuringSceneGUI(SceneView sceneView) {
+        GetFence(editFenceId).DrawGizmosLines();
     }
 
     private void OnGUI() {
-        using( new GUILayout.VerticalScope(EditorStyles.helpBox)) {
-            if(GUILayout.Button("Create Fence")) CreateFence();
+        using(new GUILayout.HorizontalScope(EditorStyles.helpBox)) {
+            newFenceName = EditorGUILayout.TextField(newFenceName);
+            if(GUILayout.Button("Create Fence")) CreateFence(newFenceName);
         }
+
+        using(new GUILayout.VerticalScope()) {
+            editFenceId = EditorGUILayout.Popup(editFenceId, GetFencesNames());
+            if(GUILayout.Button("Create Waypoint")) CreateWaypoint(GetFence(editFenceId), Vector3.zero);
+        }
+
+
+        scrollPos = GUILayout.BeginScrollView(scrollPos);
+        for(int i = 0; i < fencesMaster.transform.childCount; i++) {
+            GetFence(i).expandDisplay = EditorGUILayout.Foldout(GetFence(i).expandDisplay, GetFenceName(i));
+
+        }
+        GUILayout.EndScrollView();
+
     }
     /// <summary>
-    /// Create new Fence
+    /// Create new Fence. Create new fenceMaster if needed. 
     /// </summary>
-    public void CreateFence() {
+    /// <param name="name">Name of new fence</param>
+    /// <param name="pos">Position of new fence root</param>
+    public void CreateFence(string name, Vector3 pos) {
         if(fencesMaster != null && GameObject.Find(fenceMasterName) != null) {
             fencesMaster = GameObject.Find(fenceMasterName);
         } else {
@@ -51,6 +67,53 @@ public class FenceBuilder : EditorWindow
             fencesMaster.isStatic = true;
         }
 
+        GameObject newFence = new GameObject(name);
+        newFence.transform.SetParent(fencesMaster.transform);
+        newFence.AddComponent<Fence>();
+        newFence.transform.position = pos;
 
+    }
+
+    /// <summary>
+    /// Create new Fence. Create new fenceMaster if needed. 
+    /// </summary>
+    /// <param name="name">Name of new fence</param>
+    public void CreateFence(string name) => CreateFence(name, Vector3.zero);
+
+    /// <summary>
+    /// Creates new waypoint in given fence
+    /// </summary>
+    /// <param name="fence">Specify which fence should create waypoint</param>
+    /// <param name="pos">Position for new waypoint</param>
+    public void CreateWaypoint(Fence fence, Vector3 pos) {
+        fence.CreateWaypoint(pos);
+    }
+
+    /// <summary>
+    /// Get fence reference
+    /// </summary>
+    /// <param name="i">Fence Id</param>
+    /// <returns>Fence at given id</returns>
+    public Fence GetFence(int i) => fencesMaster.transform.GetChild(i).GetComponent<Fence>();
+
+    /// <summary>
+    /// Get name of fence at Id
+    /// </summary>
+    /// <param name="i">Fence Id</param>
+    /// <returns>Name of fence</returns>
+    public string GetFenceName(int i) => fencesMaster.transform.GetChild(i).name;
+
+    /// <summary>
+    /// Returns names of all fences
+    /// </summary>
+    /// <returns>Names of all fences</returns>
+    public string[] GetFencesNames() {
+        string[] fenceNames = new string[fencesMaster.transform.childCount];
+
+        for(int i = 0; i < fencesMaster.transform.childCount; i++) {
+            fenceNames[i] = GetFenceName(i);
+        }
+
+        return fenceNames;
     }
 }
---
+++ Assets/FenceTool/Fence.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+/// <summary>
+/// Holds data about fence
+/// </summary>
+public class Fence : MonoBehaviour
+{
+    [HideInInspector]
+    public bool expandDisplay = false;
+    /// <summary>
+    /// Creates new Waypoint at given position
+    /// </summary>
+    /// <param name="pos">Position to create waypoint</param>
+    public void CreateWaypoint(Vector3 pos) {
+        GameObject newWaypoint = new GameObject(name + " " + transform.childCount.ToString());
+        newWaypoint.transform.parent = gameObject.transform;
+        newWaypoint.transform.position = pos;
+        newWaypoint.AddComponent<FenceWaypoint>();
+    }
+    /// <summary>
+    /// Draws line beetween all waypoints
+    /// </summary>
+    public void DrawGizmosLines() {
+        if(transform.childCount > 0) {
+            Vector3[] points = new Vector3[transform.childCount];
+            for(int i = 0; i < transform.childCount; i++) {
+                points[i] = transform.GetChild(i).position;
+            }
+            //points[transform.childCount] = transform.GetChild(0).position;
+            Handles.color = Color.green;
+            Handles.DrawAAPolyLine(2f, points);
+            Handles.color = Color.white;
+        }
+    }
+}
